cf6f4ec10489eceb8737db6ae3d53e70abe4e578
emoreta <edison.moreta@bancoprocredit.com.ec>
2025-02-12 10:22:14 -0500

    ping db

--- ping-db.js
+++ ping-db.js
@@ -1,44 +1,18 @@
-const { createPool } = require('mysql2/promise');
-const { Sequelize, DataTypes } = require('sequelize');
-require('dotenv').config();
+const mysql = require('mysql');
 
-// Crear el pool de conexiones
-const pool = createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const connection = mysql.createConnection({
+  host: 'guabastudio.com', // Cambia por tu IP o dominio
+  port: 3306,
+  user: 'guabastu_app',  // Usuario de MySQL
+  password: 'Procredit2017g' // Contraseña de MySQL
 });
 
-// Configuración de Sequelize reutilizando el pool
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT|| 3306,
-  dialect: 'mysql',
-  logging: false,
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  dialectModule: require('mysql2') // Usa mysql2 para Sequelize
-});
-
-// Verificar conexión
-(async () => {
-  try {
-    const connection = await pool.getConnection();
-    console.log('¡Conexión con el pool exitosa!');
-    connection.release();
-
-    await sequelize.authenticate();
-    console.log('Sequelize también se conectó con éxito.');
-  } catch (error) {
-    console.error('Error al conectar:', error);
+connection.connect((err) => {
+  if (err) {
+    console.log(`❌ No se pudo conectar a ${connection.config.host}:${connection.config.port}`);
+    console.error('Error:', err.message);
+  } else {
+    console.log(`✅ Conexión exitosa a ${connection.config.host}:${connection.config.port}`);
   }
-})();
+  connection.end();
+});
